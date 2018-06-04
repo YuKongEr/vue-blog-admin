0a59615412cd135c38cbf30f8a527cfab96a1db3
xp541130126 <541130126@qq.com>
2018-06-04 16:12:49 +0800

    重构完成

---
+++ src/router/router.js
@@ -0,0 +1,133 @@
+import Main from '@/views/Main.vue';
+
+// 不作为Main组件的子页面展示的页面单独写，如下
+export const loginRouter = {
+    path: '/login',
+    name: 'login',
+    meta: {
+        title: 'Login - 登录'
+    },
+    component: () =>
+        import ('@/views/login.vue')
+};
+
+export const page404 = {
+    path: '/*',
+    name: 'error-404',
+    meta: {
+        title: '404-页面不存在'
+    },
+    component: () =>
+        import ('@/views/error-page/404.vue')
+};
+
+export const page403 = {
+    path: '/403',
+    meta: {
+        title: '403-权限不足'
+    },
+    name: 'error-403',
+    component: () =>
+        import ('@//views/error-page/403.vue')
+};
+
+export const page500 = {
+    path: '/500',
+    meta: {
+        title: '500-服务端错误'
+    },
+    name: 'error-500',
+    component: () =>
+        import ('@/views/error-page/500.vue')
+};
+
+export const preview = {
+    path: '/preview',
+    name: 'preview',
+    component: () =>
+        import ('@/views/form/article-publish/preview.vue')
+};
+
+export const locking = {
+    path: '/locking',
+    name: 'locking',
+    component: () =>
+        import ('@/views/main-components/lockscreen/components/locking-page.vue')
+};
+
+// 作为Main组件的子页面展示但是不在左侧菜单显示的路由写在otherRouter里
+export const otherRouter = {
+    path: '/',
+    name: 'otherRouter',
+    redirect: '/home',
+    component: Main,
+    children: [{
+            path: 'home',
+            title: {
+                i18n: 'home'
+            },
+            name: 'home_index',
+            component: () =>
+                import ('@/views/home/home.vue')
+        },
+        {
+            path: 'ownspace',
+            title: '个人中心',
+            name: 'ownspace_index',
+            component: () =>
+                import ('@/views/own-space/own-space.vue')
+        },
+        {
+            path: 'order/:order_id',
+            title: '订单详情',
+            name: 'order-info',
+            component: () =>
+                import ('@/views/advanced-router/component/order-info.vue')
+        }, // 用于展示动态路由
+        {
+            path: 'shopping',
+            title: '购物详情',
+            name: 'shopping',
+            component: () =>
+                import ('@/views/advanced-router/component/shopping-info.vue')
+        }, // 用于展示带参路由
+        {
+            path: 'message',
+            title: '消息中心',
+            name: 'message_index',
+            component: () =>
+                import ('@/views/message/message.vue')
+        }
+    ]
+};
+
+// 作为Main组件的子页面展示并且在左侧菜单显示的路由写在appRouter里
+export const appRouter = [
+
+    {
+        path: '/error-page',
+        icon: 'android-sad',
+        title: '错误页面',
+        name: 'errorpage',
+        component: Main,
+        children: [{
+            path: 'index',
+            title: '错误页面',
+            name: 'errorpage_index',
+            component: () =>
+                import ('@/views/error-page/error-page.vue')
+        }]
+    }
+];
+
+// 所有上面定义的路由都要写在下面的routers里
+export const routers = [
+    loginRouter,
+    otherRouter,
+    preview,
+    locking,
+    ...appRouter,
+    page500,
+    page403,
+    page404
+];
---
+++ src/views/login.vue
@@ -0,0 +1,112 @@
+<style lang="less">
+    @import './login.less';
+</style>
+
+<template>
+    <div class="login" @keydown.enter="handleSubmit">
+        <div class="login-con">
+            <Card :bordered="false">
+                <p slot="title">
+                    <Icon type="log-in"></Icon>
+                    欢迎登录
+                </p>
+                <div class="form-con">
+                    <Form ref="loginForm" :model="form" :rules="rules">
+                        <FormItem prop="userName">
+                            <Input v-model="form.userName" placeholder="请输入用户名">
+                            <span slot="prepend">
+                                <Icon :size="16" type="person"></Icon>
+                            </span>
+                            </Input>
+                        </FormItem>
+                        <FormItem prop="password">
+                            <Input type="password" v-model="form.password" placeholder="请输入密码">
+                            <span slot="prepend">
+                                <Icon :size="14" type="locked"></Icon>
+                            </span>
+                            </Input>
+                        </FormItem>
+                        <FormItem>
+                            <Button @click="handleSubmit" type="primary" long>登录</Button>
+                        </FormItem>
+                    </Form>
+                    <p class="login-tip">输入任意用户名和密码即可</p>
+                </div>
+            </Card>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Cookies from 'js-cookie';
+    import {
+        requestLogin
+    } from '@/api/api.js'
+    export default {
+        data() {
+            return {
+                form: {
+                    userName: 'yukong',
+                    password: ''
+                },
+                rules: {
+                    userName: [{
+                        required: true,
+                        message: '账号不能为空',
+                        trigger: 'blur'
+                    }],
+                    password: [{
+                        required: true,
+                        message: '密码不能为空',
+                        trigger: 'blur'
+                    }]
+                }
+            };
+        },
+        methods: {
+            handleSubmit() {
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+
+
+                        requestLogin({
+                            'userName': this.form.userName,
+                            'userPwd': this.form.password
+                        }).then((res) => {
+                            if (res.data.code ==
+                                1) {
+                                Cookies.set("token", res.headers.authorization);
+                                Cookies.set('user', this.form.userName);
+                                Cookies.set('password', this.form.password);
+                                this.$store.commit('setAvator',
+                                    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg'
+                                );
+                                if (this.form.userName === 'iview_admin') {
+                                    Cookies.set('access', 0);
+                                } else {
+                                    Cookies.set('access', 1);
+                                }
+                                this.$router.push({
+                                    name: 'home_index'
+                                });
+                            } else if (res.data.code == -1) {
+
+                                this.$Notice.error({
+                                    title: '用户名或者密码错误',
+                                    duration: 2
+                                });
+
+                            }
+                        })
+
+
+
+                    }
+                });
+            }
+        }
+    };
+</script>
+
+<style>
+</style>
